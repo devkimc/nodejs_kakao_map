70fcfe5af83d0d0b765b2dbce83d36fa676d8e0e
dylan <kws60000@naver.com>
2022-02-03 23:21:10 +0900

    set db-config

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,17 +1,19 @@
+const config = require('../config/db-config.json')
 const express = require('express')
 const mysql = require('mysql')
 const router = express.Router()
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'node_conn_test_db'
+    host     : config.host,
+    user     : config.user,
+    password : config.password,
+    database : config.database,
+    port     : config.port
 })
 
 router.get('/', (req, res, next) => {
     res.send('API is working properly')
-    connection.query('select  * from professor', (err, res, fields) => {
+    connection.query('select  * from user', (err, res, fields) => {
         if (err) {
             console.log(err)
         }
