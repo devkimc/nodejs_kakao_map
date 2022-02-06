ea03fee39dc093a4bfcaa261072ff1d5785c21a3
dylan <kws60000@naver.com>
2022-02-06 15:36:10 +0900

    use nodemon , babel - ES6 문법을 적용하기 위해 babel 적용 - 실시간 수정을 하기 위해 nodemon 사용

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,24 +1,33 @@
-const config = require('../config/db-config.json')
+// const config = require('../config/db-config.json')
+// const mysql = require('mysql')
 const express = require('express')
-const mysql = require('mysql')
+const getConnection = require('../app.js')
 const router = express.Router()
 
-const connection = mysql.createConnection({
-    host     : config.host,
-    user     : config.user,
-    password : config.password,
-    database : config.database,
-    port     : config.port
-})
+// const connection = mysql.createConnection({
+//     host     : config.host,
+//     user     : config.user,
+//     password : config.password,
+//     database : config.database,
+//     port     : config.port
+// })
 
-router.get('/', (req, res, next) => {
-    res.send('API is working properly')
-    connection.query('select  * from user', (err, res, fields) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log(res)
+router.get('/', (req, res) => {
+    getConnection((conn) => {
+        conn.query(
+            'SELECT * FROM USER',
+            (err, res) => {
+                if (err) throw err;
+                res.json(result);
+            }
+        )
     })
+    // connection.query('select  * from user', (err, res, fields) => {
+    //     if (err) {
+    //         console.log(err)
+    //     }
+    //     console.log(res)
+    // })
 })
 
 module.exports = router
