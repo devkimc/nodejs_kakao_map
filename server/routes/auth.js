9e586c5850b9197d29807713d165e39d2451547d
franco <kws60000@naver.com>
2022-02-16 00:22:46 +0900

    Set the data format of the result for api request

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,21 +5,35 @@ const router = Router();
 /*
     CHECK USER_INFO: GET /auth/login
 */
-router.get('/login', function(req, res) {
-    getConnection((conn) => {
-        conn.query(
-            ' SELECT 1           ' +
-            '   INFO 1           ' +
-            '   FROM USER        ' +
-            '  WHERE USER_ID = ? ' +
-            '    AND USER_PW = ? ' ,
-            [req.body.userId , req.body.userPw],
-            (err, res) => {
-                if (err) throw err;
-                res.json(res)
-            }
-        )
-    })
+router.post('/login', function(req, res) {
+  getConnection((conn) => {
+    conn.query(
+      ' SELECT USER_ID     ' +
+      '   FROM USER        ' +
+      '  WHERE USER_ID = ? ' +
+      '    AND USER_PW = ? ' ,
+      [req.body.USER_ID , req.body.USER_PW],
+      (err, result) => {
+        if (err) throw err
+				if (result.length === 0)
+				{
+					return res.status(200).json({
+						code: 20001,
+						msg: "ID와 비밀번호가 일치하지 않습니다.",
+						list: result
+					})
+				}
+				else
+				{
+					return res.status(200).json({
+						code: 10000,
+						msg: "success",
+						list: ""
+					})
+				}
+      }
+    )
+  })
 });
 
 
